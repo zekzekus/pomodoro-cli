0411f8784b908d39fb4128aade95ac59c77b7cc3
Zekeriya Koc <zekzekus@gmail.com>
2014-09-14 11:53:09 +0300

    implemented basic file based persistence for pomodoros

--- python/main.py
+++ python/main.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import pickle
+import os
 from pomodoro import PomodoroTimer
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(SCRIPT_DIR, 'data')
 
 
 def main():
-    p = PomodoroTimer()
-    p.start()
+    file_path = os.path.join(DATA_DIR, 'pomodoro.obj')
+    try:
+        f = open(file_path, 'r')
+        p = pickle.load(f)
+        f.close()
+    except IOError:
+        p = PomodoroTimer()
+    finally:
+        p.start()
+        f = open(file_path, 'w')
+        pickle.dump(p, f)
+        f.close()
 
 
 if __name__ == '__main__':

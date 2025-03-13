9057db3d98e1cc916e21960b22aaee1e89296208
Zekeriya Koc <zekzekus@gmail.com>
2025-03-13 13:19:43 +0000

    First step to migrating using types

--- python/pomodoro/__init__.py
+++ python/pomodoro/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+from typing import Optional, Type, Any
 
 from hashids import Hashids
 
@@ -16,10 +17,10 @@ ID_ALPHABET = '1234567890ABCDEF'
 
 
 class PomodoroTimer(object):
-    def __init__(self, work_duration=4, short_rest=2, long_rest=3,
-                 output_handler_cls=ProgressBarOutputHandler,
-                 backend_cls=DEFAULT_BACKEND,
-                 id=None):
+    def __init__(self, work_duration: int = 4, short_rest: int = 2, long_rest: int = 3,
+                 output_handler_cls: Type[Any] = ProgressBarOutputHandler,
+                 backend_cls: Type[Any] = DEFAULT_BACKEND,
+                 id: Optional[str] = None) -> None:
         self.id = self.__generate_id() if not id else id
         self.durations = {
             STATUS_WORK: work_duration,
@@ -32,11 +33,11 @@ class PomodoroTimer(object):
         self.session_count = 0
         self.status = STATUS_WORK
 
-    def __generate_id(self):
+    def __generate_id(self) -> str:
         hashids = Hashids(salt=ID_SALT, alphabet=ID_ALPHABET, min_length=6)
         return hashids.encrypt(int(time.time()))
 
-    def start(self):
+    def start(self) -> None:
         counter = self._counter
 
         while counter > 0:
@@ -47,27 +48,27 @@ class PomodoroTimer(object):
         self.timer_completed()
         self._next_status()
 
-    def timer_completed(self):
+    def timer_completed(self) -> None:
         if self.is_working:
             self.session_count += 1
         self.output_handler.completed(self.session_count)
 
-    def ticking(self, counter):
+    def ticking(self, counter: int) -> None:
         self.output_handler.ticking(self.status, counter, self._counter)
 
     @property
-    def is_working(self):
+    def is_working(self) -> bool:
         return self.status == STATUS_WORK
 
     @property
-    def is_short_rest(self):
+    def is_short_rest(self) -> bool:
         return self.status == STATUS_SHORT_REST
 
     @property
-    def is_long_rest(self):
+    def is_long_rest(self) -> bool:
         return self.status == STATUS_LONG_REST
 
-    def _next_status(self):
+    def _next_status(self) -> str:
         if self.is_working:
             if self.session_count % 4 == 0:
                 self.status = STATUS_LONG_REST
@@ -79,13 +80,13 @@ class PomodoroTimer(object):
         return self.status
 
     @property
-    def _counter(self):
+    def _counter(self) -> int:
         return self.durations[self.status]
 
-    def dumps(self):
+    def dumps(self) -> None:
         self.backend.dumps(self)
 
     @classmethod
-    def loads(klass, id, backend_cls=DEFAULT_BACKEND):
+    def loads(klass, id: str, backend_cls: Type[Any] = DEFAULT_BACKEND) -> 'PomodoroTimer':
         backend = backend_cls(id)
         return backend.loads()
